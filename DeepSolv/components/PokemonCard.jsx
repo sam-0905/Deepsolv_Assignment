b4c82ce0812818ad2954b8f14e543f495887833a
Allan sam <allansam0905@gmail.com>
2026-05-02 13:54:12 +0530

    feat - Added favorite btn in pokemon card with css

--- DeepSolv/components/PokemonCard.jsx
+++ DeepSolv/components/PokemonCard.jsx
@@ -3,9 +3,10 @@ import "./PokemonCard.css";
 import { useNavigate } from "react-router";
 import { useEffect, useState } from "react";
 
-const PokemonCard = ({data}) =>{
+const PokemonCard = ({data,favorites,toggleFavorite}) =>{
 
     const navigate = useNavigate();
+   const isFavorite = favorites.some((p) => p.name === data.name);
 
     return(
         <>
@@ -13,6 +14,9 @@ const PokemonCard = ({data}) =>{
             <h3 className="card-name">{data.name}</h3>
             <img src={data.image} alt={data.name} className="card-img" />
             <button className="details-btn" onClick={() => navigate(`/pokemon/${data.name}`)}>Info</button>
+            <button onClick={() => toggleFavorite(data)} className="favorite-btn">
+                {isFavorite ? "❤️" : "🤍"}
+            </button>
         </div>
         </>
     )
