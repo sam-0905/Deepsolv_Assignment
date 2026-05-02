c3b812b96a49c6c3b890293df66ed9c6ec9f092d
Allan sam <allansam0905@gmail.com>
2026-05-02 18:38:50 +0530

    Feat - Added route for header component

--- DeepSolv/App.jsx
+++ DeepSolv/App.jsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import PokemonDetails from "./pages/PokemonDetails";
 import Favorites from "./pages/Favorites";
+import Header from "./components/Header";
 
 const App = () => {
 
     return (
          <>
+         <Header/>
         <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/pokemon/:name" element={<PokemonDetails/>} />
